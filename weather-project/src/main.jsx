372345fd1a7ec8b2b41668e0bad4ddf89e268558
shaw-samina <jinxcapsule@yahoo.com>
2026-06-05 04:11:35 -0400

    add netlify toml config"

--- weather-project/src/main.jsx
+++ weather-project/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
+import "./Weather.css";
 import Weather from "./Weather.jsx";
 
 createRoot(document.getElementById("root")).render(
